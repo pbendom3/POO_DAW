a651d158b65e8bbcc8c9ced4b4ee3fbf58d254a2
A600\Patricia <p.benaventedomenec@edu.gva.es>
2026-01-20 21:15:56 +0100

    ++

--- POO_daw/src/main/java/org/example/Paciente.java
+++ POO_daw/src/main/java/org/example/Paciente.java
@@ -6,6 +6,10 @@ public class Paciente {
 
     private Random aleatorio = new Random();
     private static final char DEF_SEXO = 'X';
+    private static final int INFRAPESO = -1;
+    private static final int PESO_IDEAL = 0;
+    private static final int SOBREPESO = 1;
+    private static final int MAYOR_EDAD = 18;
 
     private String nombre;
     private int edad;
@@ -18,7 +22,7 @@ public class Paciente {
         this.nombre = nombre;
         this.edad = edad;
         this.dni = generarDNI();
-        this.sexo = sexo;
+        this.sexo = comprobarSexo(sexo);
         this.peso = peso;
         this.altura = altura;
     }
@@ -27,16 +31,112 @@ public class Paciente {
         this(null,0,DEF_SEXO,0,0);
     }
 
-    public String generarDNI(){
+    private String generarDNI(){
 
         String dni = "";
+
+        char letras[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y',
+                'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z',
+                'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+
         for (int i = 0; i < 8; i++) {
             int num = aleatorio.nextInt(10);
             dni = dni + num;
         }
 
-        return dni;
+        return dni + (letras[Integer.parseInt(dni)%23]);
 
+    }
+
+    public int calcularIMC (){
+
+        double resultado = peso / Math.pow(((double) altura/100),2);
+
+        System.out.println(resultado);
+
+        if (resultado < 20){
+            return INFRAPESO;
+        } else if (resultado > 25) {
+            return SOBREPESO;
+        }else{
+            return PESO_IDEAL;
+        }
+
+    }
+
+    public boolean esMayorDeEdad (){
+
+        if (edad >= 18){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private char comprobarSexo(char sexo){
+
+        if (sexo == 'H' || sexo == 'M'){
+            return sexo;
+        }else{
+            return DEF_SEXO;
+        }
+
+    }
+
+    public Random getAleatorio() {
+        return aleatorio;
+    }
+
+    public void setAleatorio(Random aleatorio) {
+        this.aleatorio = aleatorio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
     }
 
 
--- POO_daw/src/main/java/org/example/PacienteApp.java
+++ POO_daw/src/main/java/org/example/PacienteApp.java
@@ -4,6 +4,19 @@ public class PacienteApp {
 
     static void main() {
 
+        Paciente patri = new Paciente();
+
+        System.out.println(patri.getDni());
+
+        Paciente alejandro = new Paciente("alejandro",20,'L',100,170);
+
+        System.out.println(alejandro.getSexo());
+        System.out.println(alejandro.calcularIMC());
+
+        System.out.println(patri.esMayorDeEdad());
+        System.out.println(alejandro.esMayorDeEdad());
+
+
 
 
     }
